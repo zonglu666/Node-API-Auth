6b042dd70c35c82c2764f6a7fa206ed82adc3347
zonglu <zonglu@sheca.com>
2020-07-28 11:06:58 +0800

    commit

--- src/sign.ts
+++ src/sign.ts
@@ -18,12 +18,10 @@ function generateStringToSign(params: any = {}) {
       return 0;
     }
   }
-  console.log('params', params);
   const stringToSign = _.toPairs(params) // convert object to array [[key1, value1], [key2, value2]]
     .sort((pair1, pair2) => stringCompare(pair1[0], pair2[0])) // sort by key
     .map((pair) => pair[0] + '=' + pair[1]) // [key1=value1, key2=value2]
     .join('&'); // key1=value1&key2=value2
-  console.log('stringToSign ====== ', stringToSign);
   return stringToSign;
 }
 
@@ -32,18 +30,21 @@ function signWithPrivateKey(privateKey: string, params: any = {}) {
   const stringToSign = generateStringToSign(params);
   console.log('stringToSign ====== ', stringToSign);
   // 使用私钥签名
-  const privateKeyForClient = new NodeRSA(privateKey);
-  const signature = privateKeyForClient.sign(
-    new Buffer(stringToSign),
-    'base64',
-    'buffer'
-  );
+  const key = new NodeRSA();
+  key.importKey(privateKey, 'pkcs8-private-pem');
+  const signature = key.sign(new Buffer(stringToSign), 'base64', 'buffer');
   return signature;
 }
 
-function verifyWithPublicKey(publicKey: string, signature: string) {
-  const key = new NodeRSA(publicKey);
-  return key.verify(new Buffer(signature), signature, 'buffer', 'base64');
+function verifyWithPublicKey(
+  publicKey: string,
+  signature: string,
+  params: any
+) {
+  const stringToSign = generateStringToSign(params);
+  const key = new NodeRSA();
+  key.importKey(publicKey, 'pkcs8-public-pem');
+  return key.verify(new Buffer(stringToSign), signature, 'buffer', 'base64');
 }
 
 function main() {
@@ -58,7 +59,8 @@ function main() {
   // 使用 publicA 进行验签
   const verifyResult = verifyWithPublicKey(
     config.publicKeyForClient,
-    signature
+    signature,
+    payload
   );
   console.log('verifyResult ====== ', verifyResult);
 }
